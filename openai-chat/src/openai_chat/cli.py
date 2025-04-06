e2152ec3f64672e2b22d7d1e95ca678552bceddf
Bob Liu <akagi201@gmail.com>
2025-04-07 00:30:06 +0800

    feat: update openai chat

--- openai-chat/src/openai_chat/cli.py
+++ openai-chat/src/openai_chat/cli.py
@@ -1,20 +1,24 @@
 from openai import OpenAI
 
-client = OpenAI(
+def main():
+    client = OpenAI(
     api_key="sk-1234",
     base_url="http://127.0.0.1:3000/v1",
-)
+    )
 
-completion = client.chat.completions.create(
-    model="pai-001",
-    messages=[
-        {
-            "role": "system",
-            "content": "you are a helpful assistant that speaks Chinese.",
-        },
-        {"role": "user", "content": "Hello, from 0g user!"},
-    ],
-    temperature=0.9,
-)
+    completion = client.chat.completions.create(
+        model="phala/deepseek-r1-70b",
+        messages=[
+            {
+                "role": "system",
+                "content": "you are a helpful assistant that speaks Chinese.",
+            },
+            {"role": "user", "content": "Hello, from 0g user!"},
+        ],
+        temperature=0.9,
+    )
 
-print(completion.choices[0].message)
+    print(completion.choices[0].message)
+
+if __name__ == "__main__":
+    main()
